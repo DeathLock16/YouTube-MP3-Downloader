efa61b65121d2c84b8d3b568cf2000237c33a0c9
DeathLock16 <mjankowski1609@gmail.com>
2023-03-28 23:48:20 +0200

    Initial Commit

---
+++ YouTube-MP3-Downloadeer.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+#define COMMAND "yt-dlp -P \"/output/\" -o \"%(title)s\" --extract-audio --audio-format mp3 \""
+
+using std::string;
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
+using std::getline;
+using std::system;
+
+int main() {
+    string url;
+    cout << "Insert YouTube url:\n";
+    getline(cin, url);
+
+    int result = system((COMMAND + url + "\"").c_str());
+    if (!result)
+        cout << "File downloaded\n";
+    else cerr << "Error #" << result;
+
+    system("pause");
+    return 0;
+}
